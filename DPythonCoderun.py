481c3d5cf199a74f5a04acbdce862572fd323e4f
Aswaby14 <104822771+Aswaby14@users.noreply.github.com>
2022-05-03 02:42:29 -0500

    Add files via upload

---
+++ DPythonCoderun.py
@@ -0,0 +1,38 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/Antonnette/DPythonCode.py =================
+Author:AntonnetteSwaby
+Date:3/5/2022
+Course: ITT103
+Purpose: The Commission rate is based on two factors,the amount of sales and the class to which a salesperson belongs.
+START
+DECLARE
+sales amount Real
+sales number Interger
+sales class Interger
+Nest IFs to calculate commission
+While sales personDo
+IF class==1:
+IF sales=<1000
+Rate=0.06
+commision=sale amount * 0.06
+IF sales > 100 but < 2000
+Rate=0.07
+IF sales 2000 >
+Rate=0.1
+IF class==2:
+IF sales < 1000
+Rate=0.04
+commission=sales amount * 0.04
+IF sales 1000 >
+Rate=0.06
+commission=sales amount * 0.06
+IF class==3:
+Rate=0.045
+If class<1 or class>3:
+Error message
+commission received by sales person
+ENDIF
+ENDIF
+END
