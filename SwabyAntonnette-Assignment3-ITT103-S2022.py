4b7f7337bd6472acd699d3eafc848907835f0c34
Aswaby14 <104822771+Aswaby14@users.noreply.github.com>
2022-05-03 21:02:51 -0500

    Add files via upload

---
+++ SwabyAntonnette-Assignment3-ITT103-S2022.py
@@ -0,0 +1,41 @@
+# Author : Antonnette Swaby
+# Date : 3/5/2022
+# Course : ITT103
+# Purpose : The Commission rate is based on two factors,the amount of sales and the class to which a salesperson belongs.
+# Declar variables
+global salesAmount
+global salesNumber
+global salesClass
+def inputModule():
+    ()
+    salesAmount=Real(input("Enter monthly salesAmount."))
+    salesnumber=int(input("what is the salesnumber:"))
+    salesclass=int(input("Enter the salesclass:"))
+While ('salesperson Do')
+("class==1:")
+if sales==('<1000'):
+   'print Rate=0.06'
+coommision=('sale amount * 0.06')
+if sales > 100 < 2000:
+    'if' ('slaes 1000')
+'print Rate=0.07'
+coommision=('sale amount * 0.07')
+if sales is 2000 : >   
+'print Rate=0.1'
+coommision=('sale amount * 0.1')
+if class==2:
+if sales < 1000
+'print Rate=0.04'
+commission=('sales amount * 0.04')
+if sales 1000 >
+'print Rate=0.06'
+commission=('sales amount * 0.06')
+if class==3:
+'print Rate=0.045'
+commission=('sales amount * 0.045')
+if class<1 or class>3:
+print('Error message')
+print("commission received by sales person")
+ENDIF
+ENDIF
+END
